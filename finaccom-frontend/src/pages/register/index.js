e9253067667e8a5bddea72a6dfab320c2d0910be
Alexandre Sanva <alex_ff2@hotmail.com>
2020-03-19 09:07:54 -0300

    ajustes no react

--- finaccom-frontend/src/pages/register/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const regiter = () => (
-  <h1>regiter</h1>
-)
-
-export default regiter
--- finaccom-frontend/src/services/auth.js
+++
@@ -1,5 +0,0 @@
-export const TOKEN_KEY = "@finaccom-token"
-export const login = token => localStorage.setItem(TOKEN_KEY, token)
-export const getToken = () => localStorage.getItem(TOKEN_KEY)
-export const isAuthenticated = () => localStorage.getItem(TOKEN_KEY)
-export const logout = () => localStorage.removeItem(TOKEN_KEY)
